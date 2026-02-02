bdc61355fb2d739561a8be3a8d87ea2a09de322d
Sypher0Dronzer <sohamsaha321@gmail.com>
2026-02-02 20:29:47 +0530

    center text of statpill

--- src/components/StatPill.jsx
+++ src/components/StatPill.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const StatPill = ({ icon, name, count ,position }) => {
   const Icon=icon
   return (
-    <div className={ ` ${position} hidden sm:block -z-1 bg-linear-to-b  from-text-dark to-blackbg/20 flex flex-col items-center w-fit px-4 py-2  rounded-2xl `}>
+    <div className={ ` ${position} hidden sm:block -z-1 bg-linear-to-b  from-text-dark to-blackbg/20 flex flex-col items-center w-fit px-4 py-2  rounded-2xl text-center`}>
 
         <Icon className="size-10 text-lime" />
         <p className="font-thin">{name}</p>
-        <p className="font-semibold text-lg">{count}</p>
+        <p className="font-semibold text-lg ">{count}</p>
     </div>
   );
 };
